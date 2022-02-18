43e5c01c3fe53cb02421fded011fa9f5db558a33
Lexx Damm <lexx.damm@gmail.com>
2022-02-18 16:19:46 +0100

    Removed unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { getRepos } from './api/api';
 import { RepoList } from './components/RepoList';
 
 function App() {
